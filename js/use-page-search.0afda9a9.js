a3ef7a2cab26a241df2381f9665ed8bd3b1a65ef
tiande <1245703952@qq.com>
2023-02-20 13:03:35 +0800

    first

---
+++ js/use-page-search.0afda9a9.js
@@ -0,0 +1,1 @@
+import{d as e}from"./@vue.962d419f.js";function a(){const a=e();return{pageContentRef:a,handleResetClick:()=>{a.value?.getPageData()},handleQueryClick:e=>{a.value?.getPageData(e)}}}export{a as u};
